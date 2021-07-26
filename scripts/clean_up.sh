02ff381380aff717b5b1bf98da052b220c27cf34
Patrick McGuire <mcguirepr89@gmail.com>
2021-07-26 11:51:15 -0400

    adding a clean_up.sh script that can be run manually to remove audio files that have been processed and are over 7 days old.

---
+++ scripts/clean_up.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+source /etc/birdnet/birdnet.conf
+
+REC_DATE=$(date --date="7 days ago" "+%F")
+FIND_DATE=*$(date --date="7 days ago" "+%F")*
+
+cd "${PROCESSED}" || exit 1
+echo
+echo "	Press enter to delete the following:"
+sleep 1
+find . -name "${FIND_DATE}"
+echo
+echo "	Ctrl+c to quit and keep those files
+    or Enter to remove them"
+read
+
+find . -name "${FIND_DATE}" -exec rm -rfv {} +
