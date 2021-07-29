d6eb324698943b87127ee29a7891049bd6c4d37a
Patrick McGuire <mcguirepr89@gmail.com>
2021-07-29 18:05:21 -0400

    put the c4aarch64 license agreement at the beginning of the installation and removed the prompts from the BirdNET-system installation.

--- scripts/install_systemd.sh
+++ scripts/install_systemd.sh
@@ -18,7 +18,56 @@ case $YN in
 "\nGreat! FYI, these settings can be changed anytime by rerunning
 the configuration setup via 'sudo reconfigure_birdnet.sh'\n\n\n
 
-The next few questions will populate the required configuration settings:\n"
+Before continuing the installation, please read and accept the license
+agreement for installing and using conda4aarch64.
+
+Copyright (c) 2019 Jonathan J. Helmus
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+a. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+
+b. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the
+   distribution.
+
+c. Neither the name of the author nor the names of contributors may
+   be used to endorse or promote products derived from this software
+   without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n\n"
+
+while true;do
+  read -n1 -p \
+    "Do you agree? ["yes" to continue or "no" to exit installation]:" YN
+  echo
+  case $YN in
+	  
+    [Yy] ) break;;
+    [Nn] ) exit 1;;
+       * ) echo "I'm sorry, you can't install BirdNET-system without
+ conda4aarch64.";;
+
+  esac
+done
+
+echo "The next few questions will populate the required configuration settings:
+"
 
     read -p "1. \
  Who will be the BirdNET user? (use 'whoami' if unsure) " BIRDNET_USER
