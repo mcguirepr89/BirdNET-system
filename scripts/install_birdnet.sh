cd230a8d060c76ce8fd2f390cd3f002836c82297
Patrick McGuire <mcguirepr89@gmail.com>
2021-07-31 13:02:43 -0400

    organizing install_birdnet.sh a little better

--- scripts/install_birdnet.sh
+++ scripts/install_birdnet.sh
@@ -1,19 +1,74 @@
 #!/usr/bin/env bash
 # Install BirdNET script
 trap 'echo -e "\n\nExiting the installation. Goodbye!" && exit' SIGINT
+#Install/Configure /etc/birdnet/birdnet.conf
 my_dir=$(realpath $(dirname $0))
 cd $my_dir || exit 1
-#Install/Configure /etc/birdnet/birdnet.conf
-
 sudo ./install_systemd.sh || exit 1
 source /etc/birdnet/birdnet.conf
-
-[ -d ${RECS_DIR} ] || mkdir -p ${RECS_DIR}
-
 LASAG="https://github.com/Lasagne/Lasagne/archive/master.zip"
 THEON="https://raw.githubusercontent.com/Lasagne/Lasagne/master/requirements.txt"
 APT_DEPS=(git ffmpeg wget)
 LIBS_MODULES=(python3-pip python3-venv libblas-dev liblapack-dev)
+
+spinner() {
+  pid=$! # Process Id of the previous running command
+  
+  spin='-\|/'
+  
+  i=0
+  while kill -0 $pid 2>/dev/null
+  do
+  	  i=$(( (i+1) %4 ))
+  	    printf "\r${spin:$i:1}"
+  	      sleep .1
+  done
+}
+
+
+install_deps() {
+echo "Checking dependencies "
+sudo apt update &> /dev/null & spinner
+for i in "${LIBS_MODULES[@]}";do
+  if [ $(apt list --installed 2>/dev/null | grep "$i" | wc -l) -le 0 ];then
+    echo "Installing $i "
+    sudo apt -y install $i &> /dev/null & spinner
+  else
+    echo "$i is installed!"
+  fi
+done 
+
+for i in "${APT_DEPS[@]}";do
+  if ! which $i &>/dev/null ;then
+    echo "Installing $i "
+    sudo apt -y install $i &> /dev/null & spinner
+  else
+    echo "$i is installed!"
+  fi
+done
+}
+
+installation() {
+echo "Installing BirdNET "
+cd ~/BirdNET-system || exit 1
+echo "Setting up the birdnet  virtual environment "
+python3 -m venv birdnet & spinner
+source ./birdnet/bin/activate
+if [ ! -f "model/BirdNET_Soundscape_Model.pkl" ];then
+ echo "Fetching the model "
+ sh model/fetch_model.sh & spinner
+fi
+echo "Updating pip, wheel, and setuptools "
+pip3 install --upgrade pip wheel setuptools &> /dev/null & spinner
+echo "Installing numpy, scipy, librosa, and future "
+pip3 install -r requirements.txt &> /dev/null & spinner
+echo "Installing Theano "
+pip3 install -r "$THEON" &> /dev/null & spinner
+echo "Installing Lasagne "
+pip3 install "$LASAG" &> /dev/null & spinner
+}
+
+# START
 
 echo "This script will do the following:
 #1: Install the following BirdNET system dependencies:
@@ -36,36 +91,10 @@ press Ctrl+C to cancel. If you DO wish to install BirdNET and the
 birdnet_analysis.service, press ENTER to continue with the installation."
 echo
 echo
-echo "Checking dependencies"
-sudo apt -qqq update
-for i in "${LIBS_MODULES[@]}";do
-  if [ $(apt list --installed 2>/dev/null | grep "$i" | wc -l) -le 0 ];then
-    echo "Installing $i"
-    sudo apt -qqqy install $i
-  else
-    echo "$i is installed!"
-  fi
-done
 
-for i in "${APT_DEPS[@]}";do
-  if ! which $i &>/dev/null ;then
-    echo "Installing $i"
-    sudo apt -y install $i
-  else
-    echo "$i is installed!"
-  fi
-done
-
-cd ~/BirdNET-system || exit 1
-python3 -m venv birdnet
-source ./birdnet/bin/activate
-if [ ! -f "model/BirdNET_Soundscape_Model.pkl" ];then
- sh model/fetch_model.sh
-fi
-pip3 install --upgrade pip wheel setuptools
-pip3 install -r requirements.txt
-pip3 install -r "$THEON"
-pip3 install "$LASAG"
+[ -d ${RECS_DIR} ] || mkdir -p ${RECS_DIR} &> /dev/null
+install_deps
+installation
 
 echo "BirdNet is finished installing!!"
 echo
