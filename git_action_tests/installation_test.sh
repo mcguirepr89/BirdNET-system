f4c99ac96097d350adbec06ff962b13e00c3e59f
Patrick McGuire <mcguirepr89@gmail.com>
2021-08-02 11:39:31 -0400

    tests

--- git_action_tests/installation_test.sh
+++ git_action_tests/installation_test.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 # Testing the installation script via GitHub Actions
 ./scripts/install_birdnet.sh <<"EOF"
-y
-runner
+yrunner
 /home/runner/BirdSongs
 -80.754
 35.678
 y
 
-y
-y
-http://localhost
+yyhttp://localhost
 n
 
 n
