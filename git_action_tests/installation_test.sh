3ec91797bfe5e135420bd9fc4ca20dfcdb345599
Patrick McGuire <mcguirepr89@gmail.com>
2021-08-02 16:58:12 -0400

    adding the test to this branch

---
+++ git_action_tests/installation_test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Testing the installation script via GitHub Actions
+./scripts/install_birdnet.sh <<"EOF"
+yrunner
+/home/runner/BirdSongs
+-80.754
+35.678
+yyyhttp://localhost
+n
+n
+EOF
