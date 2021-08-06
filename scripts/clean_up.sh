aaf11e1253094c95d6af381fdceed1931d921bf9
Patrick McGuire <mcguirepr89@gmail.com>
2021-08-06 14:14:01 -0400

    fixed clean_up.sh

--- scripts/clean_up.sh
+++ scripts/clean_up.sh
@@ -1,24 +1,8 @@
 #!/usr/bin/env bash
 source /etc/birdnet/birdnet.conf
 
-<<<<<<< HEAD
-REC_DATE=$(date --date="3 days ago" "+%F")
 FIND_DATE=*$(date --date="3 days ago" "+%F")*
 
 cd "${PROCESSED}" || exit 1
-#echo
-#echo "	Press enter to delete the following:"
-#sleep 1
-#find . -name "${FIND_DATE}"
-#echo
-#echo "	Ctrl+c to quit and keep those files
-#    or Enter to remove them"
-#read
-#
-=======
 FIND_DATE=*$(date --date="2 days ago" "+%F")*
-
-cd "${PROCESSED}" || exit 1
-
->>>>>>> 9e676fe5bf0f91d29d8fe31aec4c56cfdd938e72
 find . -name "${FIND_DATE}" -exec rm -rfv {} +
