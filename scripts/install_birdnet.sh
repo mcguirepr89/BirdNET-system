a70b4ac7842590ab003cbdf87712f37516dc96ca
Patrick McGuire <mcguirepr89@gmail.com>
2021-08-08 09:06:30 -0400

    fixed indentations

--- scripts/install_birdnet.sh
+++ scripts/install_birdnet.sh
@@ -17,7 +17,7 @@ spinner() {
   pid=$! # Process Id of the previous running command
   spin='-\|/'
   i=0
-  
+
   while kill -0 $pid 2>/dev/null; do
     i=$(( (i+1) %4 ))
     printf "\r${spin:$i:1}"
@@ -37,7 +37,7 @@ install_deps() {
       echo "	$i is installed!"
     fi
   done 
-  
+
   for i in "${APT_DEPS[@]}";do
     if ! which $i &>/dev/null ;then
       echo "	Installing $i "
@@ -56,8 +56,8 @@ installation() {
   python3 -m venv birdnet 
   source ./birdnet/bin/activate
   if [ ! -f "model/BirdNET_Soundscape_Model.pkl" ];then
-   echo "	Fetching the model "
-   sh model/fetch_model.sh &> /dev/null
+    echo "	Fetching the model "
+    sh model/fetch_model.sh &> /dev/null
   fi
   echo "	Updating pip, wheel, and setuptools "
   pip3 install --upgrade pip wheel setuptools &> /dev/null
@@ -72,25 +72,25 @@ installation() {
 # START
 
 echo "	This script will do the following:
-	#1: Install the following BirdNET system dependencies:
-		- ffmpeg
-		- python3-venv
-		- python3-pip
-		- libblas-dev
-		- liblapack-dev
-		- alsa-utils (for recording)
-		- sshfs (to mount remote sound file directories)
-	#2: Creates a python virtual environment for BirdNET
-	#3: Builds BirdNET in the 'birdnet' virtual environment
-	#4: Copies the systemd .service and .mount files and enables those chosen
-	#5: Adds cron environments and jobs chosen"
+  #1: Install the following BirdNET system dependencies:
+        - ffmpeg
+        - python3-venv
+        - python3-pip
+        - libblas-dev
+        - liblapack-dev
+        - alsa-utils (for recording)
+        - sshfs (to mount remote sound file directories)
+  #2: Creates a python virtual environment for BirdNET
+  #3: Builds BirdNET in the 'birdnet' virtual environment
+  #4: Copies the systemd .service and .mount files and enables those chosen
+  #5: Adds cron environments and jobs chosen"
 
 echo
 read -sp \
-	"If you DO NOT want to install BirdNET and the birdnet_analysis.service,
-	press Ctrl+C to cancel. If you DO wish to install BirdNET and the 
-	birdnet_analysis.service, press ENTER to continue with the 
-	installation."
+  " If you DO NOT want to install BirdNET and the birdnet_analysis.service,
+  press Ctrl+C to cancel. If you DO wish to install BirdNET and the 
+  birdnet_analysis.service, press ENTER to continue with the 
+  installation."
 echo
 echo
 
@@ -101,13 +101,13 @@ installation & spinner
 echo "	BirdNet is finished installing!!"
 echo
 echo "	To start the service manually, issue:
-		'sudo systemctl start birdnet_analysis'
-	To monitor the service logs, issue:
-		'journalctl -fu birdnet_analysis'
-	To stop the service manually, issue:
-		'sudo systemctl stop birdnet_analysis'
-	To stop and disable the service, issue:
-		'sudo systemctl disable --now birdnet_analysis.service'"
+  'sudo systemctl start birdnet_analysis'
+  To monitor the service logs, issue:
+  'journalctl -fu birdnet_analysis'
+  To stop the service manually, issue:
+  'sudo systemctl stop birdnet_analysis'
+  To stop and disable the service, issue:
+  'sudo systemctl disable --now birdnet_analysis.service'"
 echo
 echo "	Enabling birdnet_analysis.service now"
 sudo systemctl enable birdnet_analysis.service
@@ -115,7 +115,7 @@ echo "	BirdNET is enabled."
 read -n1 -p "	Would you like to run the BirdNET service now?" YN
 case $YN in
   [Yy] ) sudo systemctl start birdnet_analysis.service \
-	   && journalctl -fu birdnet_analysis;;
-     * ) echo "	Thanks for installing BirdNET-system!!
-	I hope it was helpful!"; exit;;
+    && journalctl -fu birdnet_analysis;;
+* ) echo "	Thanks for installing BirdNET-system!!
+  I hope it was helpful!"; exit;;
 esac
