3ec56442b8e0b59ca8362d2a5004301a74d99e34
mcguirepr89 <60325264+mcguirepr89@users.noreply.github.com>
2021-08-07 20:36:38 -0400

    Update clean_up.sh

--- scripts/clean_up.sh
+++ scripts/clean_up.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 source /etc/birdnet/birdnet.conf
 
-FIND_DATE=*$(date --date="3 days ago" "+%F")*
-
 cd "${PROCESSED}" || exit 1
 FIND_DATE=*$(date --date="2 days ago" "+%F")*
 find . -name "${FIND_DATE}" -exec rm -rfv {} +
