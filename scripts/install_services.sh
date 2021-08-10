0e19cdaaef3a73d77793de77990e29db8b01f477
Patrick McGuire <mcguirepr89@gmail.com>
2021-08-10 17:02:05 -0400

    a read line had a missing variable

--- scripts/install_services.sh
+++ scripts/install_services.sh
@@ -147,7 +147,8 @@ EOF
 
 is_it_remote() {
   while true; do
-    read -n1 -p "Are the recordings mounted on a remote file system?"
+    read -n1 -p "Are the recordings mounted on a remote file system?" YN
+    echo
     case $YN in
       [Yy] ) echo "Checking for SSHFS to mount remote filesystem"
         if ! which sshfs &> /dev/null ;then
