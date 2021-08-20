b88bfe9e0d53951b9c2c30a018e9402db49365a8
mcguirepr89 <60325264+mcguirepr89@users.noreply.github.com>
2021-08-20 15:16:24 -0400

    Update birdnet_stats.sh

--- scripts/birdnet_stats.sh
+++ scripts/birdnet_stats.sh
@@ -29,7 +29,7 @@ while read -r line;do
   echo "    + $line"
 done < ${IDFILE}
 echo
-uptime -p | sed 's/up/Listening for/'
+echo -n "Listening since $(passwd -S | awk '{print $3}')"
 sleep 180
 clear
 done
