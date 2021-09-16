a090f79786862c1b250e490f5da2199edb0c9b7f
mcguirepr89 <60325264+mcguirepr89@users.noreply.github.com>
2021-09-16 15:56:05 -0400

    Update new_install_birdnet.sh

--- scripts/new_install_birdnet.sh
+++ scripts/new_install_birdnet.sh
@@ -17,7 +17,7 @@ fi
 
 #Install/Configure /etc/birdnet/birdnet.conf
 ./new_install_config.sh || exit 1
-HOME=${HOME} USER=${USER} sudo ./new_install_services.sh || exit 1
+sudo ./new_install_services.sh || exit 1
 source /etc/birdnet/birdnet.conf
 
 LASAG="https://github.com/Lasagne/Lasagne/archive/master.zip"
