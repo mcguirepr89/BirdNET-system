2e2c47fc626af581db9f8f7314e7c6fe147b4c02
mcguirepr89 <60325264+mcguirepr89@users.noreply.github.com>
2021-09-22 11:23:00 -0400

    Update reconfigure_birdnet.sh

--- scripts/reconfigure_birdnet.sh
+++ scripts/reconfigure_birdnet.sh
@@ -2,6 +2,6 @@
 # Reconfigure the BirdNET-system
 source /etc/birdnet/birdnet.conf
 uninstall.sh
-${HOME}/BirdNET-system/scripts/new_install_config.sh
-sudo ${HOME}/BirdNET-system/scripts/new_install_services.sh
+${HOME}/BirdNET-system/scripts/install_config.sh
+sudo ${HOME}/BirdNET-system/scripts/install_services.sh
 echo "BirdNET-system has now been reconfigured."
