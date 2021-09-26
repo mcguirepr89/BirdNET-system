88eb91445c1999d0ce5cd820a5fe56aa79aaa66e
Patrick McGuire <mcguirepr89@gmail.com>
2021-09-26 11:12:29 -0400

    testing 1 channel recording

--- scripts/birdnet_recording.sh
+++ scripts/birdnet_recording.sh
@@ -5,10 +5,10 @@ if pgrep arecord &> /dev/null ;then
   echo "Recording"
 else
   if [ -z ${REC_CARD} ];then
-    arecord -f dat -t wav --max-file-time 60 \
+    arecord -f S16_LE -c1 -r48000 -t wav --max-file-time 60 \
       --use-strftime ${RECS_DIR}/%B-%Y/%d-%A/%F-birdnet-%I:%M%P.wav
   else
-    arecord -f dat -t wav --max-file-time 60 -D "${REC_CARD}"\
+    arecord -f S16_LE -c1 -r48000 -t wav --max-file-time 60 -D "${REC_CARD}"\
       --use-strftime ${RECS_DIR}/%B-%Y/%d-%A/%F-birdnet-%I:%M%P.wav
   fi
 fi
