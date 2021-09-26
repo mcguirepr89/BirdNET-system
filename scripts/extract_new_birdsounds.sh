184d346ce7e96d48b259a6af9054632741fade64
mcguirepr89 <60325264+mcguirepr89@users.noreply.github.com>
2021-09-26 09:00:21 -0400

    Update extract_new_birdsounds.sh

--- scripts/extract_new_birdsounds.sh
+++ scripts/extract_new_birdsounds.sh
@@ -5,7 +5,7 @@ set -e
 # Keep track of the last executed command
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # Echo an error message before exiting
-trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+trap 'echo "\"${last_command}\" command exited with code $?."' EXIT
 # Remove temporary file
 trap 'rm -f $TMPFILE' EXIT
 
