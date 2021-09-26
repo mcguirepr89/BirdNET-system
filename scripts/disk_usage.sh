e9f585f7b6e24e82dd656778f731986b2a28fe5d
mcguirepr89 <60325264+mcguirepr89@users.noreply.github.com>
2021-09-26 09:08:28 -0400

    Update disk_usage.sh

--- scripts/disk_usage.sh
+++ scripts/disk_usage.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 # Disk Usage Indicator for TMUX
-df -h / \
-  | tail -n1 \
-  | awk '{print "Disk Size="$2", Used="$3", Available="$4", "$5" of disk used"}' \
+df -h / | awk '/\// {print "Disk Size="$2", Used="$3", Available="$4", "$5" of disk used"}'
