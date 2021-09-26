a72b441dc2d060faf2f457b5981c071e5ffb5982
mcguirepr89 <60325264+mcguirepr89@users.noreply.github.com>
2021-09-26 19:09:02 -0400

    Update livestream.sh

--- scripts/livestream.sh
+++ scripts/livestream.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env bash
 # Live Audio Stream Service Script
 source /etc/birdnet/birdnet.conf
-trap 'rm -f ${SOUND_PARAMS}' EXIT SIGHUP SIGINT
-SOUND_PARAMS=$(mktemp)
-SOUND_CARD="$(aplay -L \
-  | awk -F, '/^hw:/ {print $1}' \
-  | grep -ve 'vc4' -e 'Head' -e 'PCH' \
-  | uniq)"
-script -c "arecord -D ${SOUND_CARD} --dump-hw-params" -a "${SOUND_PARAMS}" &> /dev/null
-
-CHANNELS=$(awk '/CHANN/ { print $2 }' "${SOUND_PARAMS}"| sed 's/\r$//')
 
 if [ -z ${REC_CARD} ];then
   echo "Stream not supported"
